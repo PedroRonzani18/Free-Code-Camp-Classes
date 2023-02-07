4d96db0601dc2093fa29678588582b4dd6879dd7
Pedro Augusto de Portilho Ronzani <pedroaugustogabironzani@hotmail.com>
2023-02-07 00:55:41 -0300

    stack

--- Data_Structures_C_CPP/Stack/stack.cpp
+++ Data_Structures_C_CPP/Stack/stack.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <ranges>
+#include <cctype>
 
 bool balanced_parentheses(std::string str)
 {
@@ -128,7 +130,172 @@ bool balanced(std::string str)
     return st.empty();
 }
 
+int evaluate_postfix(std::string str)
+{
+    std::stack<int> stk;
+
+    for(char ch : str)
+    {
+        if(ch != '*' && ch != '/' && ch != '+' && ch != '-')
+        {
+            stk.push((int)ch - '0');
+        }
+        else
+        {
+            int n1 = stk.top(); stk.pop();
+            int n2 = stk.top(); stk.pop();
+
+            switch (ch)
+            {
+            case '*': stk.push(n2 * n1);
+                break;
+
+            case '+': stk.push(n2 + n1);
+                break;
+
+            case '-': stk.push(n2 - n1);
+                break;
+
+            case '/': stk.push(n2 / n1);
+                break;
+            }
+        }
+
+    }
+    return stk.top();
+}
+
+int evaluate_prefix(std::string str)
+{
+    std::stack<int> stk;
+
+    for (char ch : std::views::reverse(str)) 
+    {
+        std::cout << ch << std::endl;
+        if(ch != '*' && ch != '/' && ch != '+' && ch != '-')
+        {
+            stk.push((int)ch - '0');
+        }
+        else
+        {
+            int n1 = stk.top(); stk.pop();
+            int n2 = stk.top(); stk.pop();
+
+            switch (ch)
+            {
+            case '*': stk.push(n2 * n1);
+                break;
+
+            case '+': stk.push(n2 + n1);
+                break;
+
+            case '-': stk.push(n1 - n2);
+                break;
+
+            case '/': stk.push(n2 / n1);
+                break;
+            }
+
+            std::cout << stk.top() << std::endl;
+        }
+
+    }
+    return stk.top();
+}
+
+bool has_higher_precedence(char top, char ch)
+{
+    return((top == '*' || top == '/') && (ch == '+' || ch == '-'));
+}
+
+int priority (char alpha){
+    if(alpha == '+' || alpha =='-') return 1;
+    if(alpha == '*' || alpha =='/') return 2;
+    if(alpha == '^') return 3;
+    return 0;
+}
+
+std::string infix_to_postfix(std::string str)
+{
+    std::stack<char> stk;
+    std::string result = "";
+
+    for(char ch : str)
+    {
+        if(std::isdigit(ch))
+            result += ch;
+
+        else
+        {
+            while(!stk.empty() && priority(stk.top()) <= priority(ch))
+            {
+                result += stk.top();
+                stk.pop();
+            }
+            stk.push(ch);
+        }
+    }
+
+    while(!stk.empty())
+    {
+        result += stk.top();
+        stk.pop();
+    }
+
+    return result;
+}
+
+using namespace std;
+
+string infix_to_postfix(string infix)
+{
+    string postfix = "";
+    stack <char> s;
+
+    for(char ch : infix)
+    {
+        if((ch>='a' && ch<='z'|| ch>='A'&& ch<='Z') || isdigit(ch))     
+            postfix += ch;
+
+        
+        else if(ch=='(')
+            s.push(ch);
+        
+
+        else if(ch==')')
+        {
+            while(s.top()!='('){
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        
+        else            
+        {
+            while (!s.empty() && priority(ch) <= priority(s.top())){
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(ch);
+        }
+    }
+    while(!s.empty()){
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+
+
+
+
+
 int main()
 {
-    std::cout << balanced("{({[(])})}") << std::endl;
+    // std::cout << evaluate_postfix("23*54*+9-") << std::endl;
+    // std::cout << evaluate_prefix("-+*23*549") << std::endl;
+    std::cout << convert("1+2*3-4*5") << std::endl;
 }
